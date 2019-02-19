a9b029d04fd14e9771154b4d8081def9687d9aca
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-19 16:47:13 +0000

    Forwards and Backwards movement of Player implemented

---
+++ Tapletics_unity/Assets/Scripts/Subomi_move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Subomi_move : MonoBehaviour
+{
+  public float moveSpeed = 10.0f;
+
+  void FixedUpdate ()
+  {
+    float move = Input.GetAxis("Horizontal");
+
+    if (move < 0) GetComponent<Rigidbody2D>().velocity = new Vector3(move * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    if (move > 0) GetComponent<Rigidbody2D>().velocity = new Vector3(move * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+  }
+}
