187f829afc45216d2ac2faf00a889e616af35c6b
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-20 15:00:12 +0000

    Test not passing for stopwatch initialisation

---
+++ Tapletics_unity/Assets/Editor/Tests/StopWatchTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class StopWatchText
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void StopWatchTestSimplePasses()
+        {
+            // Arrange
+        
+            // Act
+
+            // Assert
+            Assert.AreEqual(stopWatchText.milliseconds, "00");
+        }
+    }
+}
