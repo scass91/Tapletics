36b9beee634b537c1d08241ea559074fe5e499f6
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-20 14:43:13 +0000

    Player moves on click

--- Tapletics_unity/Assets/Scripts/Subomi_move.cs
+++ Tapletics_unity/Assets/Scripts/Subomi_move.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Subomi_move : MonoBehaviour
-{
-  public float moveSpeed = 10.0f;
+public class Subomi_move : MonoBehaviour  {
+  public float moveForce = 200.0f;
 
   void FixedUpdate ()
   {
-    float move = Input.GetAxis("Horizontal");
+    bool run = Input.GetButtonDown("Jump");
 
-    if (move < 0) GetComponent<Rigidbody2D>().velocity = new Vector3(move * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-    if (move > 0) GetComponent<Rigidbody2D>().velocity = new Vector3(move * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    if (run) GetComponent<Rigidbody2D>().AddForce(Vector3.right * moveForce);
+
+
   }
 }
