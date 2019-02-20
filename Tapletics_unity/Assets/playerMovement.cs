69df6793fc38df0df24ad9b4d4bd909f33ebb6fe
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-20 11:38:29 +0000

    Checking if the scene we created is being pushed

---
+++ Tapletics_unity/Assets/playerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
